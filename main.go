6f7271c17730fcb3fe54b8a314c3e0b13230581a
Qais Patankar <qaisjp@gmail.com>
2017-03-18 21:21:53 +0000

    tweak

--- main.go
+++ main.go
@@ -39,6 +39,7 @@ func ws(h *Hub) {
 
 	http.HandleFunc("/stream/thisiamoausodmusdojads",
 		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 			h.broadcast <- []byte(r.Form.Get("text"))
 		},
 	)
