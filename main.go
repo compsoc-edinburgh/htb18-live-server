ddea39cf54bf7d9570076b32cc8a672a1514d0c9
Qais Patankar <qaisjp@gmail.com>
2017-03-18 20:42:57 +0000

    init

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"os"
+)
+
+var wsaddr = flag.String("ws", ":8080", "websocket address")
+
+func main() {
+	flag.Parse()
+
+	hub := newHub()
+	go hub.run()
+	go ws(hub)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		hub.broadcast <- scanner.Bytes()
+	}
+
+}
+
+func ws(h *Hub) {
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(h, w, r)
+	})
+
+	http.HandleFunc("/thisiamoausodmusdojads",
+		func(w http.ResponseWriter, r *http.Request) {
+			h.broadcast <- []byte(r.Form.Get("text"))
+		},
+	)
+	err := http.ListenAndServe(*wsaddr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
